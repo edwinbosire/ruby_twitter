6cf1a7dafa37142b96b7f59ae3df36fb207aec80
Edwin Bosire <dev@edwinb.co.uk>
2012-02-24 14:17:36 +0300

    User model with passwords and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,36 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, 
                     :format => { :with => email_regex},
                     :uniqueness => { :case_sensitive =>false}
+                    
+                    
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+
+   def encrypt_password
+     self.salt = make_salt unless has_password?(password)
+     self.encrypted_password = encrypt(password)
+   end
+
+   def encrypt(string)
+     secure_hash("#{salt}--#{string}")
+   end
+
+   def make_salt
+     secure_hash("#{Time.now.utc}--#{password}")
+   end
+
+   def secure_hash(string)
+     Digest::SHA2.hexdigest(string)
+   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,4 @@ ActiveRecord::Schema.define(:version => 20120222154836) do
     t.datetime "updated_at", :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-
 end
